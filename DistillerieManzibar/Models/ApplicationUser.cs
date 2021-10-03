df81300a4a6e8cdca732dd59bc9b08dbf3bdfd05
Florian Rampin <45884478+rampinflorian@users.noreply.github.com>
2021-10-03 22:08:41 +0200

    Add: Percentage

--- DistillerieManzibar/Models/ApplicationUser.cs
+++ DistillerieManzibar/Models/ApplicationUser.cs
@@ -6,11 +6,12 @@ namespace DistillerieManzibar.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
         public int Sold { get; set; }
         public string FullName()
         {
             return $"{FirstName} {LastName}";
         }
+
+        public int Percentage { get; set; }
     }
 }

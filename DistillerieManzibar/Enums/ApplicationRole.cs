a631c88cf704f71eacb427ac87cf71eeeabb448e
Florian Rampin <45884478+rampinflorian@users.noreply.github.com>
2021-10-04 19:45:21 +0200

    Update: Employe cast enum

---
+++ DistillerieManzibar/Enums/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DistillerieManzibar.Enums
+{
+    public enum ApplicationRole
+    {
+        [Display(Name = "Employé")]
+        Employee,
+        [Display(Name = "Patron")]
+        Boss
+    }
+}

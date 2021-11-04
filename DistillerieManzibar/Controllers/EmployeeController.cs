d315055754f381578f68f3a06f112d346f0d3c17
Florian Rampin <45884478+rampinflorian@users.noreply.github.com>
2021-11-04 20:37:57 +0100

    Add Edit %

--- DistillerieManzibar/Controllers/EmployeeController.cs
+++ DistillerieManzibar/Controllers/EmployeeController.cs
@@ -1,7 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using DistillerieManzibar.Data;
+using DistillerieManzibar.Enums;
+using DistillerieManzibar.FormsCustom;
+using DistillerieManzibar.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace DistillerieManzibar.Controllers
@@ -11,16 +16,17 @@ namespace DistillerieManzibar.Controllers
     public class EmployeeController : Controller
     {
         private readonly ApplicationDbContext _context;
+
         public EmployeeController(ApplicationDbContext context)
         {
             _context = context;
         }
-        
+
         [Route("", Name = "users.index")]
         public async Task<IActionResult> Index()
         {
             ViewBag.ApplicationUsers = await _context.ApplicationUsers.ToListAsync();
-            
+
             return View();
         }
 
@@ -38,8 +44,46 @@ namespace DistillerieManzibar.Controllers
                 return NotFound();
             }
 
-            return View(applicationUser);
+            var applicationUserFormCustom = new ApplicationUserFormCustom()
+            {
+                ApplicationUser = applicationUser,
+                ApplicationRole = ApplicationRole.Employee
+            };
 
+            return View(applicationUserFormCustom);
+        }
+
+        [Route("{id}", Name = "users.edit.post")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, ApplicationUserFormCustom applicationUserFormCustom)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(applicationUserFormCustom);
+            }
+
+            var applicationUserToUpdate = _context.ApplicationUsers.FirstOrDefault(m => m.Id == id);
+
+            if (await TryUpdateModelAsync<ApplicationUser>(applicationUserToUpdate, "ApplicationUser",
+                m => m.Percentage
+                ))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                                                 "Try again, and if the problem persists, " +
+                                                 "see your system administrator.");
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            
+            return View(applicationUserFormCustom);
         }
     }
 }
---
+++ DistillerieManzibar/FormsCustom/ApplicationUserFormCustom.cs
@@ -0,0 +1,11 @@
+﻿using DistillerieManzibar.Enums;
+using DistillerieManzibar.Models;
+
+namespace DistillerieManzibar.FormsCustom
+{
+    public class ApplicationUserFormCustom
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public ApplicationRole ApplicationRole { get; set; }
+    }
+}

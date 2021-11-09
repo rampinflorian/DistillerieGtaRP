b9cb70be02ff99814ea59030133462ab40294167
Florian Rampin <45884478+rampinflorian@users.noreply.github.com>
2021-11-09 18:34:38 +0100

    UX : Clean transactions

--- DistillerieManzibar/Controllers/TransactionController.cs
+++ DistillerieManzibar/Controllers/TransactionController.cs
@@ -26,25 +26,28 @@ namespace DistillerieManzibar.Controllers
         [Route("", Name = "transaction.index")]
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Transaction.Include(m => m.ApplicationUser).OrderByDescending(m => m.CreatedAt).ToListAsync());
+            return View(await _context.Transaction
+                .Include(m => m.ApplicationUser)
+                .Where(m => m.PayementAt == null)
+                .OrderByDescending(m => m.CreatedAt).ToListAsync());
         }
 
         [Route("create", Name = "transaction.create")]
-
         public IActionResult Create()
         {
             return View();
         }
 
         [Route("create", Name = "transaction.create.post")]
-
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TransactionId,LiquidCategory,CreatedAt,ApplicationUserId,Destination,Quantity")] Transaction transaction)
+        public async Task<IActionResult> Create(
+            [Bind("TransactionId,LiquidCategory,CreatedAt,ApplicationUserId,Destination,Quantity")]
+            Transaction transaction)
         {
             ModelState.Remove("ApplicationUserId");
             ModelState.Remove("CreatedAt");
-            
+
             if (ModelState.IsValid)
             {
                 var applicationUser = await _userManager.GetUserAsync(User);
@@ -56,38 +59,41 @@ namespace DistillerieManzibar.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
             return View(transaction);
         }
 
         [Route("edit/{id:int}", Name = "transaction.edit")]
-
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
+
             var applicationUser = await _userManager.GetUserAsync(User);
             var transaction = await _context.Transaction.FindAsync(id);
-            
+
             if (transaction == null || transaction.ApplicationUser != applicationUser)
             {
                 return NotFound();
             }
-            
+
             return View(transaction);
         }
 
         [Route("edit/{id:int}", Name = "transaction.edit")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TransactionId,LiquidCategory,CreatedAt,ApplicationUserId,Destination,Quantity")] Transaction transaction)
+        public async Task<IActionResult> Edit(int id,
+            [Bind("TransactionId,LiquidCategory,CreatedAt,ApplicationUserId,Destination,Quantity")]
+            Transaction transaction)
         {
             if (id != transaction.TransactionId)
             {
                 return NotFound();
             }
-            
+
             if (ModelState.IsValid)
             {
                 try
@@ -106,24 +112,25 @@ namespace DistillerieManzibar.Controllers
                         throw;
                     }
                 }
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(transaction);
         }
 
         [Route("delete/{id:int}", Name = "transaction.delete")]
-
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-            
+
             var applicationUser = await _userManager.GetUserAsync(User);
             var transaction = await _context.Transaction
                 .FirstOrDefaultAsync(m => m.TransactionId == id);
-            
+
             if (transaction == null || transaction.ApplicationUser != applicationUser)
             {
                 return NotFound();
@@ -133,14 +140,13 @@ namespace DistillerieManzibar.Controllers
         }
 
         [Route("delete/{id:int}", Name = "transaction.delete")]
-
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var transaction = await _context.Transaction.FindAsync(id);
             _context.Transaction.Remove(transaction);
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }

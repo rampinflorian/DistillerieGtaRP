0bfcb17fe4ce1ae5f089ba671b017a2f997fcd47
Florian Rampin <45884478+rampinflorian@users.noreply.github.com>
2021-11-12 17:54:05 +0100

    FIX : createdby and payementRecipient

--- DistillerieManzibar/Controllers/CommandController.cs
+++ DistillerieManzibar/Controllers/CommandController.cs
@@ -56,6 +56,7 @@ namespace DistillerieManzibar.Controllers
                 command.Pricing = await _context.Pricings.OrderByDescending(m => m.CreatedAt).FirstAsync(m =>
                     m.CreatedAt <= command.CreatedAt && m.CompanyId == command.CompanyId &&
                     m.LiquidCategory == command.LiquidCategory);
+                command.CreatedBy = await _userManager.GetUserAsync(User);
                 _context.Add(command);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -170,6 +171,7 @@ namespace DistillerieManzibar.Controllers
 
             var command = await _context.Commands.FindAsync(id);
             command.BilledAt = DateTime.Now;
+            command.PayementRecipient = await _userManager.GetUserAsync(User);
             _context.Update(command);
             await _context.SaveChangesAsync();
 

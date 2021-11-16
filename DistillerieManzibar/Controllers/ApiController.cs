70ac25feee05edad50011f03c6fe1cbeecdfa848
Florian Rampin <45884478+rampinflorian@users.noreply.github.com>
2021-11-16 21:52:02 +0100

    ADD : Material Design

---
+++ DistillerieManzibar/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using DistillerieManzibar.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DistillerieManzibar.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Learner, Boss, CoBoss, Leader, Employee")]
+        [Route("API")]
+        public async Task<IActionResult> GetCurrentCommandsCount()
+        {
+            return Ok(await _context.Commands.CountAsync(m => m.BilledAt == null && m.DeliveryAt == null));
+        }
+    }
+}
